ed1bece83cea774770a6601bb37295ac64f7aa73
Erik Poot Sánchez <erikpootsanchez@gmail.com>
2022-06-30 05:39:27 +0000

    Update weather settings

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'dcbe10e4806958baf826c44faf417cd9', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '21.142950',
+	defaultLongitude: '-88.151901',
 
 	// Autochange
 	autoChangeTheme: true,
